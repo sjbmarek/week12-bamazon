fed0e80fb65f6f13725a784e2c5ba99fd841bb2c
Susan Marek <smarek@chaskamn.com>
2018-02-19 08:23:22 -0600

    customer view cleanup

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,50 +20,48 @@ function listItems() {
   connection.query("SELECT * FROM bamproducts", function(err, res) {
     if (err) throw err;
     // console.log(res);
-    console.log("\tBAMAZON Items for Sale");
-    console.log("\t----------------------\n");
+    console.log("\n\n\tBAMAZON Items for Sale");
+    console.log("\t----------------------");
     for (var i = 0; i < res.length; i++) {
           console.log("\tID: " + res[i].item_id + "\tProduct: " + res[i].product_name + "\tPrice: $" + res[i].price);
     };
-    // connection.end();
+    console.log("\t----------------------\n\n");
   });
 }
 
 
 function purchaseItem() {
-  listItems();
   inquirer
     .prompt([
-     //   {
-    	// message: listItems(),
-     //    name: "shop",
-     //    type: "input",
-     //    // message: "\nWhat is the item ID you would like to purchase?"
-     //   },
+       {
+    	message: listItems(),
+        name: "shop",
+        type: "confirm",
+        message: "\nWould you like to shop?",
+       },
 
 
       {
-      	// message: listItems(),
         name: "item",
         type: "input",
-        message: "\nWhat is the item ID you would like to purchase?"
-        // validate: function(value) {
-        //     if (isNaN(value) === false) {
-        //     return true;
-        //   }
-        //   return false;
-        // }
+        message: "\nWhat is the item ID you would like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       },
       {
         name: "quantity",
         type: "input",
         message: "\nWhat quantity would you like?",
-        // validate: function(value) {
-        //   	if (isNaN(value) === false) {
-        //     return true;
-        //   }
-        //   return false;
-        // }
+        validate: function(value) {
+          	if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       }
     ])
 	.then(function(answer) {
@@ -79,28 +77,8 @@ function purchaseItem() {
 				// console.log("STOCK QTY: " + res[0].stock_quantity);
 
 			if (answer.quantity <= res[0].stock_quantity) {
-				var newStockQuantity = res[0].stock_quantity - answer.quantity;
-				// console.log("NEW STOCK NUMBER: " + newStockQuantity);
-		          connection.query(
-		            "UPDATE bamproducts SET ? WHERE ?",
-		            [
-		              {
-		                stock_quantity: newStockQuantity
-		              },
-		              {
-		                item_id: answer.item
-		              }
-		            ],
-		            function(error) {
-		              if (error) throw err;
-		              console.log("\nPURCHASE SUMMARY");
-		              console.log("________________________________________________");
-		              console.log(res[0].product_name + "\tQty: " + answer.quantity + "\tUnit Price: $" + res[0].price);
-		              console.log("\nTotal Price: $" + (res[0].price*answer.quantity));
-		              console.log("\nThank you.  Successful purchase.\n");
-		              purchaseItem();
-		            }
-		          );
+				// var newStockQuantity = res[0].stock_quantity - answer.quantity;
+		        updateProduct(res,answer);
 		    }
 		    else {
 		        console.log("\nInsfficient quantity!\n");
@@ -108,36 +86,30 @@ function purchaseItem() {
 		    };	
 			}
 		);
-
 	});
-
 };
 
+function updateProduct(res, answer) {
+  connection.query(
+    "UPDATE bamproducts SET ? WHERE ?",
+    [
+      {
+        stock_quantity: res[0].stock_quantity - answer.quantity
+      },
+      {
+        item_id: answer.item
+      }
+    ],
+    function(error) {
+      if (error) throw err;
+      console.log("\n________________________________________________");
+      console.log("PURCHASE SUMMARY\n");
+      console.log(res[0].product_name + "\tQty: " + answer.quantity + "\tUnit Price: $" + res[0].price);
+      console.log("\nTotal Price: $" + (res[0].price*answer.quantity).toFixed(2));
+      console.log("\nThank you.  Successful purchase.");
+      console.log("________________________________________________\n");
+      purchaseItem();
+    }
+  );
+}
 
-
-
-// function updateProduct() {
-//   console.log("Updating product quantities...\n");
-//   var query = connection.query(
-//     "UPDATE bamproducts SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// listItems();
-// purchaseItem();
